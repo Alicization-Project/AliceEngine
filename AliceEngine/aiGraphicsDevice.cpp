a42d63ac2f15ba444d2d9efae55bbba1267ffa62
DESKTOP-H5N22RS\good <1173653942@qq.com>
2019-05-22 23:12:28 +0800

    5.22

---
+++ AliceEngine/aiGraphicsDevice.cpp
@@ -0,0 +1,93 @@
+#include "aiGraphicsDevice.h"
+
+using namespace aiGraphics;
+
+bool GraphicsDevice::CheckCapability(GRAPHICSDEVICE_CAPABILITY capability) const
+{
+	switch (capability)
+	{
+	case aiGraphics::GraphicsDevice::GRAPHICSDEVICE_CAPABILITY_TESSELLATION:
+		return TESSELLATION;
+		break;
+	case aiGraphics::GraphicsDevice::GRAPHICSDEVICE_CAPABILITY_MULTITHREADED_RENDERING:
+		return MULTITHREADED_RENDERING;
+		break;
+	case aiGraphics::GraphicsDevice::GRAPHICSDEVICE_CAPABILITY_CONSERVATIVE_RASTERIZATION:
+		return CONSERVATIVE_RASTERIZATION;
+		break;
+	case aiGraphics::GraphicsDevice::GRAPHICSDEVICE_CAPABILITY_RASTERIZER_ORDERED_VIEWS:
+		return RASTERIZER_ORDERED_VIEWS;
+		break;
+	case aiGraphics::GraphicsDevice::GRAPHICSDEVICE_CAPABILITY_UNORDEREDACCESSTEXTURE_LOAD_FORMAT_EXT:
+		return UNORDEREDACCESSTEXTURE_LOAD_EXT;
+		break;
+	default:
+		break;
+	}
+	return false;
+}
+
+uint32_t GraphicsDevice::GetFormatStride(FORMAT value) const
+{
+	switch (value)
+	{
+	case FORMAT_R32G32B32A32_FLOAT:
+		return 16;
+	case FORMAT_R32G32_FLOAT:
+	case FORMAT_R32G32_UINT:
+	case FORMAT_R32G32_SINT:
+	case FORMAT_R16G16B16A16_FLOAT:
+		return 8;
+	case FORMAT_R11G11B10_FLOAT:
+	case FORMAT_R16G16_FLOAT:
+	case FORMAT_R16G16_UINT:
+	case FORMAT_R16G16_SINT:
+	case FORMAT_R32_FLOAT:
+	case FORMAT_R32_UINT:
+	case FORMAT_R8G8B8A8_UINT:
+	case FORMAT_R8G8B8A8_SINT:
+	case FORMAT_R8G8B8A8_UNORM:
+	case FORMAT_R8G8B8A8_SNORM:
+	case FORMAT_R10G10B10A2_UNORM:
+		return 4;
+	case FORMAT_R16_FLOAT:
+	case FORMAT_R16_UINT:
+	case FORMAT_R16_SINT:
+	case FORMAT_R16_UNORM:
+	case FORMAT_R16_SNORM:
+		return 2;
+	case FORMAT_R8_TYPELESS:
+	case FORMAT_R8_UNORM:
+	case FORMAT_R8_SNORM:
+	case FORMAT_R8_UINT:
+	case FORMAT_R8_SINT:
+	case FORMAT_A8_UNORM:
+		return 1;
+	default:
+		assert(0);
+		break;
+	}
+
+	// Probably didn't catch all...
+
+	return 16;
+}
+
+bool GraphicsDevice::IsFormatUnorm(FORMAT value) const
+{
+	switch (value)
+	{
+	case FORMAT_B8G8R8A8_UNORM:
+	case FORMAT_R8G8B8A8_UNORM:
+	case FORMAT_R10G10B10A2_UNORM:
+	case FORMAT_R16_UNORM:
+	case FORMAT_R8_UNORM:
+	case FORMAT_A8_UNORM:
+		return true;
+	}
+
+	// Probably didn't catch all...
+
+	return false;
+}
+
---
+++ AliceEngine/aiGraphicsResource.cpp
@@ -0,0 +1,179 @@
+#include "aiGraphicsResource.h"
+#include "aiGraphicsDevice.h"
+
+namespace aiGraphics
+{
+	VertexShader::~VertexShader()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyVertexShader(this);
+		}
+	}
+
+	PixelShader::~PixelShader()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyPixelShader(this);
+		}
+	}
+
+	GeometryShader::~GeometryShader()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyGeometryShader(this);
+		}
+	}
+
+	DomainShader::~DomainShader()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyDomainShader(this);
+		}
+	}
+
+	HullShader::~HullShader()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyHullShader(this);
+		}
+	}
+
+	ComputeShader::~ComputeShader()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyComputeShader(this);
+		}
+	}
+
+	Sampler::~Sampler()
+	{
+		if (device != nullptr)
+		{
+			device->DestroySamplerState(this);
+		}
+	}
+
+	GPUResource::~GPUResource()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyResource(this);
+		}
+	}
+
+	GPUBuffer::~GPUBuffer()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyBuffer(this);
+		}
+	}
+
+	VertexLayout::~VertexLayout()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyInputLayout(this);
+		}
+	}
+
+	BlendState::~BlendState()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyBlendState(this);
+		}
+	}
+
+	DepthStencilState::~DepthStencilState()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyDepthStencilState(this);
+		}
+	}
+
+	RasterizerState::~RasterizerState()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyRasterizerState(this);
+		}
+	}
+
+	void Texture::RequestIndependentRenderTargetArraySlices(bool value)
+	{
+		independentRTVArraySlices = value;
+	}
+	void Texture::RequestIndependentRenderTargetCubemapFaces(bool value)
+	{
+		independentRTVCubemapFaces = value;
+	}
+	void Texture::RequestIndependentShaderResourceArraySlices(bool value)
+	{
+		independentSRVArraySlices = value;
+	}
+	void Texture::RequestIndependentShaderResourcesForMIPs(bool value)
+	{
+		independentSRVMIPs = value;
+	}
+	void Texture::RequestIndependentUnorderedAccessResourcesForMIPs(bool value)
+	{
+		independentUAVMIPs = value;
+	}
+
+	Texture1D::~Texture1D()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyTexture1D(this);
+		}
+	}
+
+	Texture2D::~Texture2D()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyTexture2D(this);
+		}
+	}
+
+	Texture3D::~Texture3D()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyTexture3D(this);
+		}
+	}
+
+	GPUQuery::~GPUQuery()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyQuery(this);
+		}
+	}
+
+
+	GraphicsPSO::~GraphicsPSO()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyGraphicsPSO(this);
+		}
+	}
+
+	ComputePSO::~ComputePSO()
+	{
+		if (device != nullptr)
+		{
+			device->DestroyComputePSO(this);
+		}
+	}
+}
