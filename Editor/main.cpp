86429221f9d93d1dc0ca86d1f7005e5e8672f6e0
DESKTOP-H5N22RS\good <1173653942@qq.com>
2019-05-25 13:46:27 +0800

    GUI -> Object-Oriented Approach

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -3,45 +3,123 @@
 #endif 
 
 #include <windows.h>
+#include <vcruntime_new.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+template <class DERIVED_TYPE>
+class BaseWindow
+{
+public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		DERIVED_TYPE *pThis = NULL;
+
+		if (uMsg == WM_NCCREATE)
+		{
+			CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+			pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
+
+			pThis->m_hwnd = hwnd;
+		}
+		else
+		{
+			pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+		}
+		if (pThis)
+		{
+			return pThis->HandleMessage(uMsg, wParam, lParam);
+		}
+		else
+		{
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+
+	BaseWindow() : m_hwnd(NULL) { }
+
+	BOOL Create(
+		PCWSTR lpWindowName,
+		DWORD dwStyle,
+		DWORD dwExStyle = 0,
+		int x = CW_USEDEFAULT,
+		int y = CW_USEDEFAULT,
+		int nWidth = CW_USEDEFAULT,
+		int nHeight = CW_USEDEFAULT,
+		HWND hWndParent = 0,
+		HMENU hMenu = 0
+	)
+	{
+		WNDCLASS wc = { 0 };
+
+		wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
+		wc.hInstance = GetModuleHandle(NULL);
+		wc.lpszClassName = ClassName();
+
+		RegisterClass(&wc);
+
+		m_hwnd = CreateWindowEx(
+			dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+			nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
+		);
+
+		return (m_hwnd ? TRUE : FALSE);
+	}
+
+	HWND Window() const { return m_hwnd; }
+
+protected:
+
+	virtual PCWSTR  ClassName() const = 0;
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+
+	HWND m_hwnd;
+};
+
+class MainWindow : public BaseWindow<MainWindow>
+{
+public:
+	PCWSTR  ClassName() const { return L"Sample Window Class"; }
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
+
+LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(m_hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+		EndPaint(m_hwnd, &ps);
+	}
+	return 0;
+	case WM_CLOSE:
+		if (MessageBox(m_hwnd, L"Really quit?", L"Message", MB_OKCANCEL) == IDOK)
+		{
+			DestroyWindow(m_hwnd);
+		}
+		// Else: User canceled. Do nothing.
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+	}
+	return TRUE;
+}
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-	// Register the window class.
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	MainWindow win;
 
-	WNDCLASS wc = { };
-
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-
-	RegisterClass(&wc);
-
-	// Create the window.
-
-	HWND hwnd = CreateWindowEx(
-		0,                              // Optional window styles.
-		CLASS_NAME,                     // Window class
-		L"AliceEngine",    // Window text
-		WS_OVERLAPPEDWINDOW,            // Window style
-
-		// Size and position
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-		NULL,       // Parent window    
-		NULL,       // Menu
-		hInstance,  // Instance handle
-		NULL        // Additional application data
-	);
-
-	if (hwnd == NULL)
+	if (!win.Create(L"AliceEngine", WS_OVERLAPPEDWINDOW))
 	{
 		return 0;
 	}
 
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(win.Window(), nCmdShow);
 
 	// Run the message loop.
 
@@ -53,34 +131,4 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 	}
 
 	return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-
-
-
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-		EndPaint(hwnd, &ps);
-	}
-	return 0;
-	case WM_CLOSE:
-		if (MessageBox(hwnd, L"Really quit?", L"Message", MB_OKCANCEL) == IDOK)
-		{
-			DestroyWindow(hwnd);
-		}
-		// Else: User canceled. Do nothing.
-		return 0;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
